38b781f8f842c7b83426367e325da4954de2a697
HammamiKhairi <hammamikhairi@gmail.com>
2022-11-13 19:09:24 +0100

    simple script using 1.5 mil words

--- main.go
+++ main.go
@@ -1,44 +1,79 @@
 package main
 
 import (
-	. "example/mymodule/Radixtree/radix"
+	"encoding/csv"
+	Radix "example/mymodule/Radixtree/radix"
 	"fmt"
+	"os"
+	"time"
 )
+
+func getData() [][]string {
+	f, err := os.Open("data.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
 
 func main() {
 
-	//initialize tree
-	tree := TreeInit()
+	data := getData()
 
-	// add words
-	tree.Addword("aban")
-	tree.Addword("cabi")
-	tree.Addword("caba")
-	tree.Addword("cabaa")
-	tree.Addword("czbaa")
-	tree.Addword("khairi")
-	tree.Addword("khairis")
-	tree.Addword("khkkris")
-	tree.Addword("khkklis")
+	//initialize tree
+	tree := Radix.TreeInit()
+
+	for i, line := range data {
+		if i > 0 {
+			tree.Addword(line[0])
+		}
+	}
 
 	// prints the tree (currently unhuman)
-	fmt.Println("------- Print Tree -------")
-	tree.Print()
+	// fmt.Println("------- Print Tree -------")
+	// tree.Print()
 
-	// checks if a word exists
-	fmt.Println("------- Word Checks -------")
-	fmt.Println(tree.SearchTree("k"))       // -> false
-	fmt.Println(tree.SearchTree("kh"))      // -> false
-	fmt.Println(tree.SearchTree("kha"))     // -> false
-	fmt.Println(tree.SearchTree("khai"))    // -> false
-	fmt.Println(tree.SearchTree("khair"))   // -> false
-	fmt.Println(tree.SearchTree("khairi"))  // -> true
-	fmt.Println(tree.SearchTree("khairis")) // -> true
+	// auto completion based on initial strings
 
-	// auto completion based on initial strings (not finalized ( no checks ) )
-	fmt.Println("------- Auto Completion \"c\" -------")
-	tree.AutoComplete("c")
+	fmt.Println("------- Confirm that every word in the dataset is in the tree -------")
+	for i, line := range data {
+		if i > 0 {
+			if !tree.SearchTree(line[0]) {
+				panic("A word is missing")
+			}
+		}
+	}
+	fmt.Println("All checks are good")
 
-	fmt.Println("------- Auto Completion \"kh\" -------")
-	tree.AutoComplete("kh")
+	fmt.Println("------- Auto Completion (no pre checks implemented yet) -------")
+	var input string
+	var start time.Time
+	var end time.Duration
+	for {
+		fmt.Print("~ search : ")
+		fmt.Scanf("%s", &input)
+
+		if input == "" {
+			fmt.Println("you cant do that :(")
+			continue
+		}
+		// fmt.Print("------- Auto Completing \"")
+		// fmt.Print(input)
+		// fmt.Print("\" -------\n")
+
+		start = time.Now()
+		number := tree.AutoComplete(input)
+		end = time.Since(start)
+		fmt.Printf("%d matches found in ", number)
+		fmt.Println(end)
+	}
 }
